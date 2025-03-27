b350d3b9d556e6dccb1e52b82fe75f9b6a5e09f5
Praveen Akash Kadhiravan <Praveenakashk@ducontindia.local>
2025-03-27 13:44:43 +0530

    label updated

--- src/ModelViewer.jsx
+++ src/ModelViewer.jsx
@@ -12,24 +12,24 @@ const GridFloor = () => {
     return <gridHelper args={[10, 10, "#999", "#444"]} position={[0, -1.01, 0]} />;
 };
 
-const UIOverlay = ({ percentage }) => {
+const UIOverlay = ({ percentage, handleClick }) => {
     return (
         <>
-            {/* Green Display Panel - Adjusted to match first container */}
-            <mesh position={[3, 2, 0]} rotation={[0, 0, 0]}>  
-                <planeGeometry args={[0.4, 0.3]} />  
+
+            <mesh position={[3, 2, 0]} rotation={[0, 0, 0]} onClick={handleClick}
+                onPointerOver={(e) => (e.stopPropagation(), document.body.style.cursor = "pointer")}
+                onPointerOut={(e) => (e.stopPropagation(), document.body.style.cursor = "default")}>
+                <planeGeometry args={[0.4, 0.3]} />
                 <meshStandardMaterial color="lime" />
             </mesh>
 
-            {/* Percentage Display Box (Black background) */}
             <Billboard position={[-3, 2, 0]}>
-                {/* Percentage Display Box (Black background) */}
                 <mesh>
-                    <planeGeometry args={[0.3, 0.2]} />
+                    <planeGeometry args={[0.5, 0.2]} />
                     <meshStandardMaterial color="black" />
                 </mesh>
 
-                {/* Dynamic Percentage Text */}
+
                 <Text
                     fontSize={0.15}
                     color="white"
@@ -46,6 +46,10 @@ const UIOverlay = ({ percentage }) => {
 const ModelViewer = () => {
     const [percentage, setPercentage] = useState(0);
 
+    const handleIncrease = () => {
+        setPercentage((prev) => Math.min(prev + 10, 100));
+    };
+
     return (
         <div style={{ width: "100%", height: "100vh", position: "relative" }}>
             <Canvas
@@ -57,29 +61,22 @@ const ModelViewer = () => {
                 camera={{ position: [2, 3, 8], fov: 50 }}
                 shadows
             >
-                {/* Ambient Light */}
                 <ambientLight intensity={1} />
 
-                {/* Directional Light */}
                 <directionalLight position={[5, 5, 5]} intensity={4} castShadow />
 
-                {/* Spotlight for depth */}
                 <spotLight position={[10, 10, 10]} angle={0.3} intensity={1.5} castShadow />
 
-                {/* Grid Floor */}
                 <GridFloor />
 
-                {/* 3D Model */}
                 <Model />
 
-                {/* UI Overlays */}
-                <UIOverlay percentage={percentage} />
+                <UIOverlay percentage={percentage} handleClick={handleIncrease} />
 
-                {/* Camera Controls */}
                 <OrbitControls enableDamping={true} dampingFactor={0.1} />
             </Canvas>
 
-           
+
         </div>
     );
 };
